ea82a111711ee02552aecd9643b9517eb5957ab4
zulma <zumendez7@gmail.com>
2020-12-28 15:06:24 +0100

    variables, reafectation et booleans

---
+++ public/js/main.js
@@ -0,0 +1,35 @@
+let phrase = "j'aime les tartines et j'en mange ";
+console.log(phrase);
+
+let nombre = "4 par jour";
+
+let conca = phrase + nombre;
+console.log(conca);
+
+let chiffre = 5;
+let chiffre2 = 35;
+
+console.log(chiffre + chiffre2);
+console.log(phrase + " " + chiffre);
+
+// reafectation
+let rea1 = "salut tout le monde";
+console.log(rea1);
+
+let rea2 = "au revoir";
+console.log(rea2);
+
+rea1 = rea2; 
+console.log(rea1)
+
+// Opérateurs de grandeur, les égalités, la différence et les Booleans
+
+let num = 4;
+let mot = "6";
+
+console.log(num > mot);
+console.log(num < mot);
+console.log(num == mot);  
+console.log(num != mot);  
+console.log(num === mot);
+console.log(num !== mot); 
